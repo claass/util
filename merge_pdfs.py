8013ba5977b93ab6e5fb4edb5d607ea98dadba42
Lennard Claassen <ldc@lennards-mbp.lan>
2018-12-25 15:53:06 -0800

    added CLI

--- merge_pdfs.py
+++ merge_pdfs.py
@@ -1,16 +1,46 @@
+import argparse
 import contextlib
 import PyPDF2
+import glob
 
 
-def merge_pdfs(pdfs, output_file):
+def parse_args():
+    """Get CLI arguments passed by user"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--input_folder",
+        required=True,
+        help="path to input folder",
+        type=str
+    )
+    parser.add_argument(
+        "-o",
+        "--output_file",
+        required=True,
+        help="path and name for output (incl. extension)",
+        type=str
+    )
+    args = parser.parse_args()
+    return args
+
+
+def merge_pdfs(input_folder, output_file):
     """
-    Merges PDF files. Hotfixing bug in current version of PyPDF2
+    Merges PDF files in specified folder in alphabetical order.
+    Hotfixes a bug in current version of PyPDF2 that resulted in blank pages.
 
     Args:
-        pdfs (list, str):   paths to pdfs that need to be merged
-        output (str):       path to output file
+        input_folder (str): paths to pdfs that need to be merged
+        output_file  (str): path to output file (including extension)
     """
 
+    # Getting paths for pdf files only
+    pdfs = glob.glob(input_folder+"*.pdf")
+    pdfs.sort()
+
+    # Merge PDFs by appending and save to output path
     with contextlib.ExitStack() as stack:
         pdfMerger = PyPDF2.PdfFileMerger()
         files = [stack.enter_context(open(pdf, 'rb')) for pdf in pdfs]
@@ -19,4 +49,8 @@ def merge_pdfs(pdfs, output_file):
         with open(output_file, 'wb') as f:
             pdfMerger.write(f)
 
-## TODO: Add if __name__ == '__main__': section
+
+if __name__ == '__main__':
+    arguments = parse_args()
+    merge_pdfs(arguments.input_folder, arguments.output_file)
+    print('Merge complete.')
--- split_csv.py
+++ split_csv.py
@@ -2,24 +2,6 @@ import pandas as pd
 import argparse
 
 
-def split_csv(input_file, output_folder, output_file, split_by):
-    """
-    Splits csv file by values in a specfied column and
-    stores them in the output folder.
-
-    Args:
-        input_file (str):   path to csv to be split
-        output_folder (str):  folder to save chunks to
-        output_file (str):  prefix for output chunks
-        split_by (str):     column name to split the input csv by
-    """
-    df = pd.read_csv(input_file)
-    for val in df[split_by].unique():
-        sub_df = df[df[split_by] == val]
-        filename = output_folder + output_file + '__' + val + ".csv"
-        sub_df.to_csv(filename, index=False)
-
-
 def parse_args():
     """Get CLI arguments passed by user"""
 
@@ -54,6 +36,25 @@ def parse_args():
     )
     args = parser.parse_args()
     return args
+
+
+def split_csv(input_file, output_folder, output_file, split_by):
+    """
+    Splits csv file by values in a specfied column and
+    stores them in the output folder.
+
+    Args:
+        input_file (str):   path to csv to be split
+        output_folder (str):  folder to save chunks to
+        output_file (str):  prefix for output chunks
+        split_by (str):     column name to split the input csv by
+    """
+    
+    df = pd.read_csv(input_file)
+    for val in df[split_by].unique():
+        sub_df = df[df[split_by] == val]
+        filename = output_folder + output_file + '__' + val + ".csv"
+        sub_df.to_csv(filename, index=False)
 
 
 if __name__ == '__main__':
